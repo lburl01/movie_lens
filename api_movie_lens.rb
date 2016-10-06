7ef54582cc169664eb42d402e9262c454dbc4cc2
Lori Burleson <lori.burleson@gmail.com>
2016-10-06 18:15:15 -0400

    Added lots of files for creating local instances on other computers and preparing for deploying to heroku

--- api_movie_lens.rb
+++ api_movie_lens.rb
@@ -1,4 +1,19 @@
 require 'sinatra'
+require 'active_record'
+require 'yaml'
+require_relative 'character'
+
+# database_config = YAML::load(File.open('config/database.yml'))
+
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+
+before do
+  content_type :json
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
 
 get 'api/movie/1' do
   JSON.dumps '{"id": 1, "title": "Toy Story"}'
