c5769421e444bafb34751e378833b4360309ae53
Lori Burleson <lori.burleson@gmail.com>
2016-10-06 18:11:19 -0400

    Updating all the things

---
+++ db/migrate/001_create_user_table.rb
@@ -0,0 +1,24 @@
+
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def up
+    create_table :users do |t|
+      t.integer :age
+      t.string :gender
+      t.string :occupation
+      t.integer :zip_code
+    end
+  end
+
+  def drop
+    drop_table :users
+  end
+end
+
+def main
+  action = (ARGV[0] || :up).to_sym
+
+  CreateUsers.migrate(action)
+
+end
+
+main if __FILE__ == $PROGRAM_NAME
