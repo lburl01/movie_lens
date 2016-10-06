ca3ea7e76b901bf90de6db5ba15812368f73c384
Lori Burleson <lori.burleson@gmail.com>
2016-10-06 17:52:22 -0400

    Program will populate all tables (including joined) correctly

--- movies_seed.rb
+++ movies_seed.rb
@@ -10,27 +10,28 @@ def main
     title = line[1]
     release_date = line[2]
     video_release = line[3]
-    unknown_genre = line[4].to_i
-    action = line[5].to_i
-    adventure = line[6].to_i
-    animation = line[7].to_i
-    children = line[8].to_i
-    comedy = line[9].to_i
-    crime = line[10].to_i
-    documentary = line[11].to_i
-    drama = line[12].to_i
-    fantasy = line[13].to_i
-    film_noir = line[14].to_i
-    horror = line[15].to_i
-    musical = line[16].to_i
-    mystery = line[17].to_i
-    romance = line[18].to_i
-    sci_fi = line[19].to_i
-    thriller = line[20].to_i
-    war = line[21].to_i
-    western = line[22].to_i
+    imdb_url = line[4]
+    unknown_genre = line[5].to_i
+    action = line[6].to_i
+    adventure = line[7].to_i
+    animation = line[8].to_i
+    children = line[9].to_i
+    comedy = line[10].to_i
+    crime = line[11].to_i
+    documentary = line[12].to_i
+    drama = line[13].to_i
+    fantasy = line[14].to_i
+    film_noir = line[15].to_i
+    horror = line[16].to_i
+    musical = line[17].to_i
+    mystery = line[18].to_i
+    romance = line[19].to_i
+    sci_fi = line[20].to_i
+    thriller = line[21].to_i
+    war = line[22].to_i
+    western = line[23].to_i
 
-    Movie.create!(id: id, title: title, release_date: release_date, video_release: video_release, unknown_genre: unknown_genre, action: action, adventure: adventure, animation: animation, children: children, comedy: comedy, crime: crime, documentary: documentary, drama: drama, fantasy: fantasy, film_noir: film_noir, horror: horror, musical: musical, mystery: mystery, romance: romance, sci_fi: sci_fi, thriller: thriller, war: war, western: western)
+    Movie.create!(id: id, title: title, release_date: release_date, video_release: video_release, imdb_url: imdb_url, unknown_genre: unknown_genre, action: action, adventure: adventure, animation: animation, children: children, comedy: comedy, crime: crime, documentary: documentary, drama: drama, fantasy: fantasy, film_noir: film_noir, horror: horror, musical: musical, mystery: mystery, romance: romance, sci_fi: sci_fi, thriller: thriller, war: war, western: western)
 
   end
 
---
+++ ratings_seed.rb
@@ -0,0 +1,19 @@
+require 'csv'
+require_relative 'environment'
+require_relative 'model'
+require_relative 'schema'
+
+def main
+  csv = CSV.read('u.data', encoding: 'windows-1252', col_sep: "\t")
+  csv.each do |line|
+    user_id = line[0].to_i
+    movie_id = line[1].to_i
+    rating = line[2].to_f
+    timestamp = line[3]
+
+    Rating.create!(user_id: user_id, movie_id: movie_id, rating: rating, timestamp: timestamp)
+
+  end
+
+end
+main if __FILE__ == $PROGRAM_NAME
