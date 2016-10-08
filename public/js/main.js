b91b5c48ae1e7d40e69d20c8fc19480b76ec20b5
Andrew Toups <mistertoups@gmail.com>
2016-10-08 19:25:20 -0400

    Added movie genre method

--- public/js/main.js
+++ public/js/main.js
@@ -95,10 +95,13 @@ function round(value, precision) {
 // CONSTRUCTORS:
 
 function Movie(dataObject) {
+  console.log(dataObject);
   this.id = dataObject.id;
   this.title = dataObject.title;
+  this.date = dataObject.release_date;
+  this.imbd = dataObject.imdb_url;
+  this.genre = this.getGenre(dataObject);
   this.getAverage();
-  console.log(this);
 }
 
 Movie.prototype = {
@@ -106,11 +109,40 @@ Movie.prototype = {
     var average;
     $.ajax(apiGet('ratingAverage', this.id)).done((function(response, average){
       average = round(response, 1);
-      console.log("Inside callback:");
-      console.log(average);
       this.average = average;
-      console.log(this);
     }).bind(this));
+  },
+  getGenre: function(dataObject){
+    var genreDictionary = {
+      "unknown_genre":0,
+      "action":0,
+      "adventure":0,
+      "animation":0,
+      "children":0,
+      "comedy":0,
+      "crime":0,
+      "documentary":0,
+      "drama":0,
+      "fantasy":0,
+      "film_noir":0,
+      "horror":0,
+      "musical":0,
+      "mystery":0,
+      "romance":0,
+      "sci_fi":0,
+      "thriller":0,
+      "war":0,
+      "western":0
+    };
+    var genres = '';
+    for (var keyName in dataObject){
+      for (var genre in genreDictionary){
+        if (keyName === genre && dataObject[keyName] == 1){
+          genres += genre + "\n";
+        }
+      }
+    }
+    return genres;
   }
 };
 
